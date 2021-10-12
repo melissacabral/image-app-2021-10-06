40fce9bd9ea7b02abe4282c39b32d0f891ec794e
Melissa Cabral <melissacabral@gmail.com>
2021-10-12 09:48:39 -0700

    Set up the home page and basic stylesheet

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Image App Demo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+This is just the beginning
+</body>
+</html>
